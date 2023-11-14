13f85b075a274ee2c8ccb67f68741b7d1efffdfc
Voold <barsik.koteevich@mail.ru>
2023-11-14 15:04:59 +0700

    tiny update again

--- JS/main.js
+++ JS/main.js
@@ -18,9 +18,9 @@ function ButRcl(el){
 	step+=215;
 	if (step >= 215)
 		ButL.style.visibility="visible";
-	if (step <= 1075)
+	if (step <= 1290)
 		ed_row.style.cssText="right: +"+step+"px;transition: 0.6s linear;";
-	else if (step <= 1290){
+	else if (step <= 1505){
 		el.style.visibility="hidden";
 		ed_row.style.cssText="right: +"+step+"px;transition: 0.6s linear;";
 	}
