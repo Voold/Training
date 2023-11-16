21785a428a04d27af0e4278868cab188abf30523
Voold <barsik.koteevich@mail.ru>
2023-11-16 15:14:10 +0700

    animation fix

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
 var ed_row = document.getElementById("ed_row");
 var ButR = document.getElementById("ButR");
 var ButL = document.getElementById("ButL");
-var Count=0;
+var Count = 0;
 var step = 0;
 
 function testes() {
@@ -15,20 +15,20 @@ function but1(el){
 
 
 function ButRcl(el){
-	step+=215;
+	step+=220;
 	if (step >= 215)
 		ButL.style.visibility="visible";
-	if (step <= 1290)
+	if (step <= 1100)
 		ed_row.style.cssText="right: +"+step+"px;transition: 0.6s linear;";
-	else if (step <= 1505){
+	else if (step <= 1320){
 		el.style.visibility="hidden";
 		ed_row.style.cssText="right: +"+step+"px;transition: 0.6s linear;";
 	}
 }
 
 function ButLcl(el){
-	if (step >= 215){
-		step-=215;
+	if (step >= 220){
+		step-=220;
 		ButR.style.visibility="visible";
 	}
 	if (step == 0){
