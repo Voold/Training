937b5521e2bd09e5fadbfdabe05e1eea2392dd95
Voold <barsik.koteevich@mail.ru>
2023-12-05 14:48:04 +0700

    tiny fixes

--- JS/form.js
+++ JS/form.js
@@ -4,14 +4,14 @@ name = "login";
 
 
 function checkForm(el){
-	var name = el.name.value;
+	var tname = el.name.value;
 	var pass = el.pass.value;
 	
 	var fail = "";
 	
-	if (name == "" || pass == "")
+	if (tname == "" || pass == "")
 		fail="Заполните все поля";
-	else if (name.length<3 || name.length >50)
+	else if (tname.length<3 || name.length >50)
 		fail="Длина имени должна быть от 3 до 50 символов";
 	
 	if (fail != "")
