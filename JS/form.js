801964c2709c6b7cbf11b1c357b21eacd992962b
Voold <barsik.koteevich@mail.ru>
2023-12-05 14:45:10 +0700

    tiny fixes

--- JS/form.js
+++ JS/form.js
@@ -1,4 +1,24 @@
+export {name};
 var name;
 name = "login";
-name = document.getElementById("name");
-export {name};
+
+
+function checkForm(el){
+	var name = el.name.value;
+	var pass = el.pass.value;
+	
+	var fail = "";
+	
+	if (name == "" || pass == "")
+		fail="Заполните все поля";
+	else if (name.length<3 || name.length >50)
+		fail="Длина имени должна быть от 3 до 50 символов";
+	
+	if (fail != "")
+		alert (fail);
+	else{
+		name = document.getElementById("name");
+		window.location = "../pages/index.html";
+	}
+	return false;
+}
