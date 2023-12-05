f5cd73b619101c611ba23db1541dd5136629b1a3
Voold <barsik.koteevich@mail.ru>
2023-12-06 00:15:12 +0700

    test update

--- JS/form.js
+++ JS/form.js
@@ -3,25 +3,24 @@ name = "login";
 
 
 function Go(el){
-	var tname = document.getElementById("name").value;
-	var pass = document.getElementById("pass").value;
+	name = el.name.value;
+	var pass = el.pass.value;
 	
 	var fail = "";
 	
-	el.style.background= "red";
-	
-	if (tname == "" || pass == "")
+	if (name == "" || pass == "")
 		fail="Заполните все поля";
-	else if (tname.length<3 || tname.length >20)
+	else if (name.length<3 || tname.length >20)
 		fail="Длина имени должна быть от 3 до 20 символов";
 	
 	if (fail != "")
 		alert (fail);
 	else{
-		name = tname;
 		alert (name);
 		window.location = "../index.html";
 	}
+	
+	return false;
 }
 
 export {name};
